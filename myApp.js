c9ec0763fdb5498ea68ddd2a8054acba2da27634
Vishal Prasad <30768888+Vishal0Prasad@users.noreply.github.com>
2023-07-16 20:44:32 +0000

    wip: API practice

--- myApp.js
+++ myApp.js
@@ -1,5 +1,19 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
+app.use("/public",express.static(__dirname+"/public"));
+
+app.get("/",function(req, res) {
+    res.sendFile(__dirname+"/views/index.html");
+  })
+
+app.get("/json",function(req,res){
+    console.log(process.env.MESSAGE_STYLE);
+    res.json({
+        "message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" :"Hello json"
+    })
+})
 
 
 
